be2554d2cfdac72b3b93e71bcbfb311d9552cc1e
ArchangelumC <153145786+ArchangelumC@users.noreply.github.com>
2024-01-28 21:33:49 -0300

    Adjustments

--- Engenharia de Software – Desafio Angelo Camargo - PYTHON.py
+++ Engenharia de Software – Desafio Angelo Camargo - PYTHON.py
@@ -1,8 +1,21 @@
+import gdown
 import pandas as pd
 
-# Reading of sheet data
-sheet_location = 'Desafio.xlsx'#https://docs.google.com/spreadsheets/d/12RpmtrCdXpxcwh6-ROoUw7GhfKo1235ZJF084d9iVAM/edit#gid=800279462
-dados = pd.read_excel(sheet_location)
+# Specifying the Google Drive file URL
+drive_url = "https://docs.google.com/spreadsheets/d/12ORB7DwpwZTpAs8aItQWBGIGYESGFCx1/edit?usp=sharing&ouid=101996123968694005289&rtpof=true&sd=true"
+
+# Get the file ID from the Google Drive URL
+file_id = drive_url.split('/')[-2]
+
+# Specify the local file path where you want to save the downloaded Excel file
+local_file_path = "Desafio.xlsx"
+
+# Download the file using gdown
+gdown.download(f"https://drive.google.com/uc?id={file_id}", local_file_path, quiet=False)
+
+print(f"File downloaded successfully to {local_file_path}")
+
+dados = pd.read_excel(local_file_path)
 
 # Operation to verify the approval situation for each student
 for indice, linha in dados.iterrows():
@@ -13,7 +26,7 @@ for indice, linha in dados.iterrows():
     P3 = linha['P3']
 
     media = (P1 + P2 + P3) / 3
-#Operation to fill up the sheets with the results and situations
+    #Operation to fill up the sheets with the results and situations
     if faltas > 15: #Coresponds at 25% of the Total number of Classes
         dados.at[indice, 'Situação'] = "Reprovado por Falta"
         dados.at[indice, 'Nota para Aprovação Final'] = 0
@@ -26,5 +39,6 @@ for indice, linha in dados.iterrows():
     else:
         dados.at[indice, 'Situação'] = "Reprovado por Nota"
         dados.at[indice, 'Nota para Aprovação Final'] = 0
+
 #Saves the DataFrame in the sheet
 dados.to_excel('Desafio.xlsx', index=False)
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # TesteTuntsRocks
 Teste Admissional Tunts Rocks <br/>
 1.Certify to have python installed in your computer <br/>
-2.In case of missing the library 'panda' use the command 'pip install pandas'<br/>
+2.In case of missing the library 'pandas' and 'gdown' use the command 'pip install pandas' and 'pip install gdown'<br/>
 3.Certify to have a spreadsheet with columns named 'Aluno', 'P1','P2', 'P3', 'Situação', 'Nota para Aprovação Final'<br/>
-4.Rename the file to 'Desafio.xlsx' <br/>
-5.To run the program use a python IDE and use the command 'Shift + F10' in python(Certify to have the sheet file closed) or execute in a terminal using python Engenharia de Software - Desafio Angelo Camargo.py <br/>
-6.That will fill up the sheet with the new values and results<br/>
-7.Open your sheet file.
+4.To run the program use a python IDE and use the command 'Shift + F10' in python(Certify to have the sheet file closed) or execute in a terminal using python Engenharia de Software - Desafio Angelo Camargo.py <br/>
+5.That will download the sheet with the new values and results<br/>
+6.Open your sheet file.<BR/>
+LINK TO THE SHEET:
+https://docs.google.com/spreadsheets/d/12ORB7DwpwZTpAs8aItQWBGIGYESGFCx1/edit?usp=sharing&ouid=101996123968694005289&rtpof=true&sd=true
+
 
 
